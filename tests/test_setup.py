7875b5fe225d9b8ac2ff84c708471881a9d4a8dc
Shuhao Wu <shuhao@shuhaowu.com>
2014-02-15 21:48:06 -0500

    Fixed tests.

--- tests/test_setup.py
+++ tests/test_setup.py
@@ -4,17 +4,18 @@ import unittest
 
 from .utils import FlaskTestCase
 
+
 class TestBasicRoutes(FlaskTestCase):
   def test_basic_routes(self):
     self.assertStatus(200, self.get("/"))
     self.assertStatus(200, self.get("/features"))
-    self.assertRedirect("/", self.get("/app"))
+    self.assertRedirect("/", self.get("/app/"))
 
   def test_login_logout(self):
     self.login()
-    self.assertStatus(200, self.get("/app"))
+    self.assertStatus(200, self.get("/app/"))
     self.logout()
-    self.assertRedirect("/", self.get("/app"))
+    self.assertRedirect("/", self.get("/app/"))
 
 if __name__ == "__main__":
   unittest.main()
