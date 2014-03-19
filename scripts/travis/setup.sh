d657cf2666a08d3d1d6f66de85424ffdb47f490c
Shuhao Wu <shuhao@shuhaowu.com>
2014-03-19 16:45:13 -0400

    Trying again.

--- scripts/travis/setup.sh
+++ scripts/travis/setup.sh
@@ -9,7 +9,7 @@ sudo service riak start
 mkdir -p userfiles
 
 pip install -r requirements.txt
-sudo npm install -g karma karma-cli karma-jasmine
+sudo npm install -g karma karma-cli karma-jasmine karma-phantomjs-launcher
 
 cat >settings_local.py <<EOL
 DEBUG = True
