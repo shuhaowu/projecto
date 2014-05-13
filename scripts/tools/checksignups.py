8d1db29dbead72f76b47a017c6b1c7546780a05c
Shuhao Wu <shuhao@shuhaowu.com>
2014-05-13 18:05:36 -0400

    Updated checking script.

--- scripts/tools/checksignups.py
+++ scripts/tools/checksignups.py
@@ -5,5 +5,9 @@ if __name__ == "__main__":
   rc = riak.RiakClient(protocol="pbc", nodes=settings.RIAK_NODES)
   signup_bucket = rc.bucket(settings.DATABASES["signups"])
 
+  l = 0
   for email in signup_bucket.stream_keys():
     print email
+    l += 1
+
+  print "Total:", l, "signups"
