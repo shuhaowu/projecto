003a528802318910d7be8ae4a507d7b894fc45f1
Shuhao Wu <shuhao@shuhaowu.com>
2014-05-13 21:08:22 -0400

    Updated so that static files can be separated!

--- Gruntfile.js
+++ Gruntfile.js
@@ -28,7 +28,7 @@ module.exports = function(grunt) {
   var ALL_CSS_SOURCES = [
     "static/css/base.css",
     "static/css/app.css",
-    "projecto/apiv1/**/static/css/*.js"
+    "projecto/apiv1/**/static/css/*.css"
   ];
 
   grunt.initConfig({
@@ -89,6 +89,40 @@ module.exports = function(grunt) {
         }
       }
     },
+    copy: {
+      prod: {
+        files: [
+          {
+            cwd: "static/fonts/",
+            src: "**",
+            dest: "prod_static/fonts/",
+            flatten: true,
+            expand: true
+          },
+          {
+            cwd: "static/img/",
+            src: "**",
+            dest: "prod_static/img/",
+            flatten: true,
+            expand: true
+          },
+          {
+            cwd: "static/js/vendor/",
+            src: "**",
+            dest: "prod_static/js/vendor/",
+            flatten: true,
+            expand: true
+          },
+          {
+            cwd: "static/js/",
+            src: "foundation.min.js",
+            dest: "prod_static/js/",
+            flatten: true,
+            expand: true
+          }
+        ],
+      }
+    },
     watch: {
       jswatch: {
         files: ALL_JS_SOURCES,
@@ -102,7 +136,8 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-copy');
 
   grunt.registerTask("default", ["jshint", "karma:unit:start", "watch"]);
-  grunt.registerTask("prod", ["uglify:prod", "cssmin:prod", "karma:produnit"])
+  grunt.registerTask("prod", ["uglify:prod", "cssmin:prod", "copy:prod", "karma:produnit"]);
 };
